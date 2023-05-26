cf2ed2f2771401abe53fd23c48ea4aaf048596bb
Santiago Developer <Santiago Developer@DESKTOP-S2D4PQE.fibertel.com.ar>
2023-05-26 09:33:41 -0300

    Ejercicio n#2 de Actividades afianciamiento. Escribir un programa que solicite al usuario que escriba su nombre.

---
+++ src/main/java/com/mycompany/practicas/Practicas.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.practicas;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Santiago Developer
+ */
+public class Practicas {
+    public static void main(String[] args) {
+        
+        String nombre;
+        String apellido;
+        int dni;
+        
+        Scanner leer = new Scanner(System.in);
+        System.out.println(" Ingrese su nombre ");
+        nombre = leer.next();
+        
+        System.out.println(" Bienvenido " + nombre );
+        System.out.println(" Ingrese su apellido ");
+        apellido = leer.next();
+       
+        System.out.println( " Ingrese su  Dni " );
+        dni = leer.nextInt();
+        
+        System.out.println(" Usted ha ingresado al Sistema ");
+      
+   
+        
+     
+        
+        
+       
+                
+                
+                
+    }
+    }
+
